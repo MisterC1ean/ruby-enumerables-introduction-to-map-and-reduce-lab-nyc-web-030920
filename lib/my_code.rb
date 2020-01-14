13c2248352e82d0e115ea767d77d9c4d4667e4c0
Robert Keller <robert_keller44@icloud.com>
2020-01-14 05:40:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,12 +41,11 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point)
-  
   total = 0
-  while starting_point < source_array.length
+  counter = 0
+  while counter < source_array.length
   total = total + source_array[counter]
   counter += 1
 end
 total
-  
 end
