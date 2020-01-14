b169c1f13d9f9e003b308eaf616ee17c2075e7ee
Robert Keller <robert_keller44@icloud.com>
2020-01-14 05:53:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,12 +43,9 @@ end
 def reduce_to_total(source_array, starting_point)
   counter = 0
   total = 0
-  if starting_point
-    total = starting_point
-  end
   while counter < source_array.length
-      total = total + source_array[counter]
+      total = total + source_array[counter] 
   counter += 1
 end
-total
+total + starting_point
 end
